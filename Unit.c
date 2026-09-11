fa1bc84c09fedb41cc1ae64cd44f187e315b6352
“Alvingimwa-git” <alvingimwa@gmail.com>
2026-09-11 17:47:30 +0300

    Wrote to display semester units using C

---
+++ Unit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Units offered this semester:\n");
+    printf("1. Digital literacy\n");
+    printf("2. Communication skills\n");
+    printf("3. Structured programming and algorithms\n");
+    printf("4. Basic mathematics\n");
+    printf("5. Computational logic\n");
+    printf("6. Introduction to Physics\n");
+    printf("7. Health and Wellness Management\n");
+    return 0;
+}

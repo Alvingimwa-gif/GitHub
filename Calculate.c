db0eedd91a81d091634a4da4e667e00e6f9ae6f0
“Alvingimwa-git” <alvingimwa@gmail.com>
2026-09-17 12:28:25 +0300

    Program for volume and surface area calculation
    This program uses C to calculate the volume and surface area of a cylinder with the user required to input the radius and the height

---
+++ Calculate.c
@@ -0,0 +1,28 @@
+ /*
+    Author: Alvin
+    Reg Number: BCS-05-0068/2026
+    Description:Program to calculate the volume and surface area of a cylinder using C
+    Date: 12/9/2026
+    Version 1
+    */
+
+#include <stdio.h>
+#define π 3.14159
+
+int main() {
+    float radius, height;
+    double volume, surface_area;
+
+    printf("Enter the radius of the cylinder: ");
+    scanf("%f", &radius);
+    printf("Enter the height of the cylinder: ");
+    scanf("%f", &height);
+
+    volume = π * radius * radius * height;
+    surface_area = 2 * π * radius * radius + 2 * π * radius * height;
+
+    printf("Volume of the cylinder: %.2lf\n", volume);
+    printf("Surface area of the cylinder: %.2lf\n", surface_area);
+
+    return 0;
+}
